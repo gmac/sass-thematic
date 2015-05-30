0463476bdac130df1b9356eec8c0b7dba036b198
Greg MacWilliam <gmacwill77@gmail.com>
2015-05-30 18:12:48 -0400

    Adding compilation rules.

--- README.md
+++ README.md
@@ -2,4 +2,48 @@
 
 [pre-release]
 
-This is a work in progress to combine [file-importer](https://github.com/gmac/file-importer) with [gonzales-pe](https://github.com/tonyganch/gonzales-pe), resulting in a complete Sass abstract syntax tree reconstituted from deeply-nested sources.
+Reconstitute an entire Sass filebase into an Abstract Syntax Tree (AST) of lexically-parsed grammar. This is useful for performing validations and/or extracting portions of your Sass styleset for specific purposes.
+
+## How it works
+
+This is an extremely lightweight library that combines [file-importer](https://github.com/gmac/file-importer) with the fabulous [gonzales-pe](https://github.com/tonyganch/gonzales-pe) CSS lexer. Here's the secret sauce:
+
+ * `file-importer` is used to reconstitute the file tree of a Sass codebase, combining files referenced via `@import` statements.
+
+ * As files are imported, each file is parsed into an AST by `gonzales`, and then merged into its parent AST in place of its `@import` rule.
+
+The net result is a complete `gonzales` AST object, composed of deeply-nested source trees.
+
+## Install
+
+To install in your NPM package:
+
+```
+npm install sass-ast --save-dev
+```
+
+## Usage
+
+```javascript
+var sassAST = require('sass-ast');
+
+sassAST.parse({
+    file: 'lib/test',
+    includePaths: ['./styles/']
+  },
+  function(err, ast) {
+    if (err) throw err;
+    console.log(ast);
+  });
+```
+
+### sassAST.parse( options, callback )
+
+## Issues
+
+This library is a tiny bridge between two primary tools that do the heavy lifting. Most issues should probably be reported directly to their specific project:
+
+ * Report issues with `@import` resolutions to [file-importer](https://github.com/gmac/file-importer).
+ * Report issues with Sass lexical parsing to [gonzales-pe](https://github.com/tonyganch/gonzales-pe).
+
+Thanks! Reporting directly to the appropriate project will make sure your report is reviewed.
--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 var gonzales = require('gonzales-pe');
 var File = require('file-importer');
 
+function extend(base, obj) {
+  for (var i in obj) {
+    if (obj.hasOwnProperty(i)) base[i] = obj[i];
+  }
+  return base;
+}
+
+/**
+ * Customized compiler for assembling AST data.
+ * Loaded file dependencies will have their ASTs precompiled.
+ * This compiler wants to aggregate loaded ASTs into one payload.
+ */
+File.prototype.compile = function(files) {
+  if (files.length === 1) {
+    // Inherit loaded data and AST directly:
+    this.data = files[0].data;
+    this.ast = files[0].ast;
+  }
+  else {
+    // Compile aggregate data and AST wrapper.
+    // This AST is a stylesheet with many nested stylesheets.
+    this.data = '';
+    this.ast = gonzales.createNode({
+      type: 'stylesheet',
+      content: [],
+      start: [0, 0],
+      end: [0, 0]
+    });
+
+    // Use this after the next gonzales release:
+    //this.ast = gonzales.parse('', {syntax: 'scss'});
+
+    for (var i = 0; i < files.length; i++) {
+      this.data += files[i].data + '\n';
+      this.ast.content.push(files[i].ast);
+    }
+  }
+
+  return this;
+};
 
 File.prototype.parse = function(done) {
-  //console.log(this.file);
+  // Parse the file's AST.
+  // File may already have AST data from previous compile step.
+  this.ast = this.ast || gonzales.parse(this.data, {syntax: 'scss'});
+  this.ast.file = this.file;
+
+  console.log('parse:', this.file);
+
   var self = this;
-  var ast = self.ast = gonzales.parse(self.data, {syntax: 'scss'});
+  var ast = this.ast;
   var imports = {};
   var nodes = {};
 
@@ -14,13 +60,13 @@ File.prototype.parse = function(done) {
     var node = ast.content[i];
 
     // If a root node is an @rule
-    if (node.type === 'atrules') {
+    if (node.is('atrules')) {
       // Attempt to match an @import statement string:
       var match = node.toCSS('scss').match(/@import\s+['"]([^'"]+)['"]/);
 
       if (match) {
         var file = match[1];
-        var delimiter = ast.content[i+1];
+        var nextNode = ast.content[i+1];
 
         // Create a file for the import, if we need one:
         if (!imports[file]) {
@@ -32,7 +78,7 @@ File.prototype.parse = function(done) {
         nodes[file].push(node);
 
         // Remove any subsequent delimiter:
-        if (delimiter && delimiter.type === 'declarationDelimiter') {
+        if (nextNode && nextNode.is('declarationDelimiter')) {
           ast.content.splice(i+1, 1);
         }
       }
@@ -46,10 +92,12 @@ File.prototype.parse = function(done) {
     if (file && nodes[file.file]) {
       for (var i=0; i < nodes[file.file].length; i++) {
         var node = nodes[file.file][i];
-        node.type = file.ast.type;
-        node.content = file.ast.content;
+
+        // Extend node with all properties of the imported AST:
+        for (var prop in file.ast) {
+          if (file.ast.hasOwnProperty(prop)) node[prop] = file.ast[prop];
+        }
       }
-      console.log(this.ast);
     }
 
     for (var i in imports) {
@@ -66,9 +114,16 @@ File.prototype.parse = function(done) {
   return this;
 };
 
+
+module.exports.parse = function(opts, done) {
+  new File(opts).render(function(err, file) {
+    done(err, file.ast);
+  });
+};
+
 function doit(opts) {
   new File(opts).render(function(err, file) {
-    console.log(file);
+    console.log(JSON.stringify(file.ast, null, '  '));
   });
 }
 
