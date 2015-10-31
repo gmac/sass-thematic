ce14738aee271030b0d6ed6d5f797f7220b74e5f
Greg MacWilliam <gmacwill77@gmail.com>
2015-10-31 16:19:57 -0400

    Thematic engine as base API object.

--- index.js
+++ index.js
@@ -1,79 +1,85 @@
 var AST = require('./lib/ast');
 var Thematic = require('./lib/thematic');
 
-module.exports = {
-  // parseAST
+// parseAST
 
-  parseAST: function(opts, done) {
-    AST.parse(opts, function(err, result) {
-      done(err, result.ast);
-    });
-  },
-  parseASTSync: function(opts) {
-    return AST.parseSync(opts).ast;
-  },
-
-  // parseThemeAST
-
-  parseThemeAST: function(opts, done) {
-    AST.parse(opts, function(err, result) {
-      var theme = new Thematic(result.ast, opts).parse(opts);
-      done(err, theme.ast);
-    });
-  },
-  parseThemeASTSync: function(opts) {
-    var result = AST.parseSync(opts);
-    var theme = new Thematic(result.ast, opts).parse(opts);
-    return theme.ast;
-  },
-
-  // parseThemeSass
-
-  parseThemeSass: function(opts, done) {
-    this.parseThemeAST(opts, function(err, ast) {
-      done(err, ast.toString());
-    });
-  },
-  parseThemeSassSync: function(opts) {
-    return this.parseThemeASTSync(opts).toString();
-  },
-
-  // parseTemplateSass
-
-  parseTemplateSass: function(opts, done) {
-    opts.template = true;
-    opts.disableTreeRemoval = true;
-    this.parseThemeAST(opts, function(err, ast) {
-      done(err, ast.toString());
-    });
-  },
-  parseTemplateSassSync: function(opts) {
-    opts.template = true;
-    opts.disableTreeRemoval = true;
-    return this.parseThemeASTSync(opts).toString();
-  },
-
-  // renderThemeCSS
-
-  renderThemeCSS: function(opts, done) {
-    AST.parse(opts, function(err, result) {
-      new Thematic(result.ast, opts).css(opts, done);
-    });
-  },
-  renderThemeCSSSync: function(opts) {
-    var result = AST.parseSync(opts);
-    return new Thematic(result.ast, opts).cssSync(opts);
-  },
-
-  // renderThemeTemplate
-
-  renderThemeTemplate: function(opts, done) {
-    AST.parse(opts, function(err, result) {
-      new Thematic(result.ast, opts).template(opts, done);
-    });
-  },
-  renderThemeTemplateSync: function(opts) {
-    var result = AST.parseSync(opts);
-    return new Thematic(result.ast, opts).templateSync(opts);
-  }
+Thematic.parseAST = function(opts, done) {
+  AST.parse(opts, function(err, result) {
+    done(err, result.ast);
+  });
 };
+
+Thematic.parseASTSync = function(opts) {
+  return AST.parseSync(opts).ast;
+};
+
+// parseThemeAST
+
+Thematic.parseThemeAST = function(opts, done) {
+  AST.parse(opts, function(err, result) {
+    var theme = new Thematic(result.ast, opts).parse(opts);
+    done(err, theme.ast);
+  });
+};
+
+Thematic.parseThemeASTSync = function(opts) {
+  var result = AST.parseSync(opts);
+  var theme = new Thematic(result.ast, opts).parse(opts);
+  return theme.ast;
+};
+
+// parseThemeSass
+
+Thematic.parseThemeSass = function(opts, done) {
+  this.parseThemeAST(opts, function(err, ast) {
+    done(err, ast.toString());
+  });
+};
+
+Thematic.parseThemeSassSync = function(opts) {
+  return this.parseThemeASTSync(opts).toString();
+};
+
+// parseTemplateSass
+
+Thematic.parseTemplateSass = function(opts, done) {
+  opts.template = true;
+  opts.disableTreeRemoval = true;
+  this.parseThemeAST(opts, function(err, ast) {
+    done(err, ast.toString());
+  });
+};
+
+Thematic.parseTemplateSassSync = function(opts) {
+  opts.template = true;
+  opts.disableTreeRemoval = true;
+  return this.parseThemeASTSync(opts).toString();
+};
+
+// renderThemeCSS
+
+Thematic.renderThemeCSS = function(opts, done) {
+  AST.parse(opts, function(err, result) {
+    new Thematic(result.ast, opts).css(opts, done);
+  });
+};
+
+Thematic.renderThemeCSSSync = function(opts) {
+  var result = AST.parseSync(opts);
+  return new Thematic(result.ast, opts).cssSync(opts);
+};
+
+// renderThemeTemplate
+
+Thematic.renderThemeTemplate = function(opts, done) {
+  AST.parse(opts, function(err, result) {
+    new Thematic(result.ast, opts).template(opts, done);
+  });
+};
+
+Thematic.renderThemeTemplateSync = function(opts) {
+  var result = AST.parseSync(opts);
+  return new Thematic(result.ast, opts).templateSync(opts);
+};
+
+module.exports = Thematic;
